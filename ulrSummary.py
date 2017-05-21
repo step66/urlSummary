915ed71fc8c3ee2f8a5dc3a7eca6e47e246106ce
step66 <red.mailhub@gmail.com>
2017-05-21 19:04:25 +0800

    Create ulrSummary.py

---
+++ ulrSummary.py
@@ -0,0 +1,11 @@
+
+import re
+from pyteaser import SummarizeUrl
+url = raw_input('\nEnter url to be summarized:\n')
+print ('\nSummarizing... \n')
+summaries = SummarizeUrl(url)
+print ('The Summary:\n')
+print summaries
+print ('\n')
+print ('Done. \n')
+
